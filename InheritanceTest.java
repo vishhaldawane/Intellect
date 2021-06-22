6b932b7f1963a1276bda7754316ab848fbd225d0
vishhaldawane <45403067+vishhaldawane@users.noreply.github.com>
2021-06-22 13:07:11 +0530

    Add files via upload

--- InheritanceTest.java
+++ InheritanceTest.java
@@ -10,7 +10,7 @@ class BankAccount
 	}
 
 	/* void set */BankAccount(int num, String holder, double bal) {
-    	System.out.println("Setting the BankAccount details...");
+    	System.out.println("BankAccount(int,String,double) ctor...");
     	accountNumber=num;
     	accountHolderName=holder;
     	accountBalance=bal;
@@ -25,17 +25,56 @@ class BankAccount
 }
 class SavingsAccount extends BankAccount 
 {
+	float rateOfInterest; //4th data member + inherited 3 fields 
+	
+	SavingsAccount(float rate) {
+		super(); //default first line
+		System.out.println("SavingsAccount(float)....");
+		rateOfInterest = rate;
+	}
+	SavingsAccount(int num, String holder, double bal, float rate) {
+		super(num,holder,bal); // reuse the existing code to initialize
+		//accountNumber= num;
+		System.out.println("SavingsAccount(int,String,double,float)....");
+		rateOfInterest = rate;
+	}
+	
+	void showBankAccount() { //ACCESSOR METHOD
+		super.showBankAccount(); //invoke super class method
+    	System.out.println("Account rate    : "+rateOfInterest);
+    	System.out.println("----------------------------------");
+    }
 	// implicit no-arg ctor | default ctor	
 }
 class FixedDepositAccount extends SavingsAccount
 {
-	// implicit no-arg ctor | default ctor
+	int maturityYear;
+	
+	FixedDepositAccount(int num, String holder, double bal, float rate, int year) {
+		super(num,holder,bal,rate);
+		maturityYear = year;
+	}
+	void showBankAccount() { //ACCESSOR METHOD
+		super.showBankAccount(); //invoke super class method
+    	System.out.println("Account Maturity: "+maturityYear);
+    	System.out.println("----------------------------------");
+    }
 }
 public class InheritanceTest {
 	public static void main(String[] args) {
+		
 		BankAccount bankObj = new BankAccount(101, "Julie", 50000);
 		bankObj.showBankAccount();	
-		bankObj.setBankAccount(106, "Julia", 45000);
-		bankObj.showBankAccount();
+		
+		System.out.println("===============");
+		
+		SavingsAccount sa = new SavingsAccount(102,"Smith",60000,3.5f);
+		sa.showBankAccount();
+		
+		System.out.println("===============");
+		
+		FixedDepositAccount fd = new FixedDepositAccount(103, "Peter", 70000, 4.5f, 2025);
+		fd.showBankAccount();
+		
 	}
 }
