0fea0cacd7cf794bf951ef17b3a1a2465d8ec525
vishhaldawane <45403067+vishhaldawane@users.noreply.github.com>
2021-06-26 10:53:59 +0530

    Add files via upload

--- HashSetTest.java
+++ HashSetTest.java
@@ -25,7 +25,93 @@ class Book
 		return "Book [bookNumber=" + bookNumber + ", bookName=" + bookName + ", bookAuthor=" + bookAuthor
 				+ ", bookEdition=" + bookEdition + ", bookCost=" + bookCost + ", numberOfPages=" + numberOfPages + "]";
 	}
+	@Override
+	public int hashCode() { // artificial number generation 
+				// would be same if the contens are same
+		
+		final int prime = 31;
+		
+		int result = 1;					//JG - 55
+		result = prime * result + ((bookAuthor == null) ? 0 : bookAuthor.hashCode());
+		
+				//31   *  81 = 2511 + 1200 = 3711
+		result = prime * result + Float.floatToIntBits(bookCost); //1200
+		
+			   //	31  * 3711 + 115041 + 4      
+		result = prime * result + bookEdition; //4 -> 115045
+		
+					//31 * 115045 = 3566395 + 10 
+		result = prime * result + ((bookName == null) ? 0 : bookName.hashCode()); //Java
+		
+			//	 31 * 3566405 + 101 = 3566506
+		result = prime * result + bookNumber; //101
+		
+		//31 * 3566506 = 110561686 + 1000 = 110562686 
+		result = prime * result + numberOfPages; //1000
+		return result;
+	}  
+	/*
+	 * Book b1 = new Book(101, "The Java Programming Language", "James Gosling", 4, 1200,1000);
+	 * Book b1 = new Book(101, "The Java Programming Language", "Kathey S", 4, 1200,1000);
+	 * 
+	 * 
+	   Book b2 = new Book(102, "The C++ Programming Language", "Bjarne Stroustup", 2, 800,1500);
+			
+	 * 
+	 */
 	
+	@Override
+	public boolean equals(Object obj) {
+		//why the boolean is by default false :
+		
+		//"false" does not require "the proof" to prove it "as a false"
+		
+		//It is BAD to "feel bad" to "remove bad habits"
+		//It is GOOD to "feel good" to "improve good habits"
+		
+		
+		//the court needs a proof to prove as truth 
+		
+		if (this == obj) // shelf.add(b1); shelf.add(b1);
+			return true;
+		
+		if (obj == null) // shelf.add(null);
+			return false;
+		
+		if (getClass() != obj.getClass())  //shelf.add(b1); shelf.add(accObj);
+			return false;
+		
+		Book other = (Book) obj; //casting into a Book
+		
+		if (bookAuthor == null) {
+			if (other.bookAuthor != null)
+				return false;
+			
+		} else if (!bookAuthor.equals(other.bookAuthor))
+			return false; // indication to hashSet to take it as a unique object
+		
+		
+		if (Float.floatToIntBits(bookCost) != Float.floatToIntBits(other.bookCost))
+			return false;
+		
+		if (bookEdition != other.bookEdition)
+			return false;
+		
+		if (bookName == null) {
+			if (other.bookName != null)
+				return false;
+			
+		} else if (!bookName.equals(other.bookName))
+			return false;
+		
+		if (bookNumber != other.bookNumber)
+			return false;
+		
+		if (numberOfPages != other.numberOfPages)
+			return false;
+		
+		return true; //to come here, all the fields must be same
+	}
 	
 	
 }
@@ -39,8 +125,20 @@ public class HashSetTest {
 			Book b4 = new Book(104, "The Autobiography Of A Yogi", "SS Paramhansa Yogananda", 1, 175, 730);
 			Book b5 = new Book(104, "The Autobiography Of A Yogi", "SS Paramhansa Yogananda", 1, 175, 730);
 			Book b6 = new Book(104, "The Autobiography Of A Yogi", "SS Paramhansa Yogananda", 1, 175, 730);
+			//b5 and b6 are added, since hashcodes are different
+			//but content is same!!!! 
+			//how do we inform hashset's add() algorithm ???? 
+			//identify unique ness based on hashcode() + equals()
 			
-			Book b7 = b4;
+			Book b7 = b4; // book wont be added again, since hashcode of 
+			//both b7 and b4 is the same
+			/*System.out.println("b1 "+b1.hashCode());
+			System.out.println("b2 "+b2.hashCode());
+			System.out.println("b3 "+b3.hashCode());
+			System.out.println("b4 "+b4.hashCode());
+			System.out.println("b5 "+b5.hashCode());
+			System.out.println("b6 "+b6.hashCode());
+			System.out.println("b7 "+b7.hashCode());*/
 			
 		System.out.println("Content is ready........");
 		
@@ -55,9 +153,9 @@ public class HashSetTest {
 		myBookShelf.add(b3);
 		
 		myBookShelf.add(b4);
-		myBookShelf.add(b5);
-		myBookShelf.add(b6);
-		myBookShelf.add(b7);
+		myBookShelf.add(b5); //it is the same book
+		myBookShelf.add(b6); //it is the same book
+		myBookShelf.add(b7); //it is the same book
 		
 		
 	
