ce248ecc240141521c547291edf0b78c8d2cc1ae
vishhaldawane <45403067+vishhaldawane@users.noreply.github.com>
2021-06-29 17:41:47 +0530

    Add files via upload

--- EmployeeEnquiryServlet.java
+++ EmployeeEnquiryServlet.java
@@ -68,100 +68,153 @@ public class EmployeeEnquiryServlet extends GenericServlet {
 
 	
 	public void service(ServletRequest request, ServletResponse response) throws ServletException, IOException {
+		
 		try
 		{
 			System.out.println("\tservice()...Host : "+request.getRemoteHost()+" Port : "+request.getRemotePort());
 			PrintWriter pw = response.getWriter();
 			
 			String whatIsClicked = request.getParameter("submit"); //read the value of the submit button
-			String whatIsEmpno = request.getParameter("myempno"); // read the value of the input type
 			
-			System.out.println("What is clicked "+whatIsClicked);
-			System.out.println("What is empno   "+whatIsEmpno);
-			
-			int eno = Integer.parseInt(whatIsEmpno);
-			
-			st = conn.createStatement();
-			rs = st.executeQuery("select * from emp WHERE empno="+eno);
-			
-			//4 process it
-			if(rs.next()) 
-			{ //if the record is found
+			if(whatIsClicked.equals("Find Employee"))
+			{
+				String whatIsEmpno = request.getParameter("myempno"); // read the value of the input type
+				System.out.println("What is clicked "+whatIsClicked);
+				System.out.println("What is empno   "+whatIsEmpno);
+				int eno = Integer.parseInt(whatIsEmpno);
+				st = conn.createStatement();
+				rs = st.executeQuery("select * from emp WHERE empno="+eno);
+				//4 process it
+				if(rs.next()) 
+				{ //if the record is found
+					int empno 	 = rs.getInt(1);
+					String ename = rs.getString(2);
+					String job   =  rs.getString(3);
+					int mgr      =  rs.getInt(4);
+					java.sql.Date date = rs.getDate(5);
+					float salary = rs.getFloat(6);
+					float comm   = rs.getFloat(7);
+					int deptno   = rs.getInt(8);
+					System.out.println("EMPNO  : "+empno);
+					System.out.println("NAME   : "+ename);
+					System.out.println("JOB    : "+job);
+					System.out.println("MGR    : "+mgr);
+					System.out.println("DOJ    : "+date);
+					System.out.println("SAL    : "+salary);
+					System.out.println("COMM   : "+comm);
+					System.out.println("DEPTNO : "+deptno);
+					System.out.println("-----------------------------");	
 				
-				int empno 	 = rs.getInt(1);
-				String ename = rs.getString(2);
-				String job   =  rs.getString(3);
-				int mgr      =  rs.getInt(4);
-				java.sql.Date date = rs.getDate(5);
-				float salary = rs.getFloat(6);
-				float comm   = rs.getFloat(7);
-				int deptno   = rs.getInt(8);
+					pw.println("<html>");
+						pw.println("<title>Employee Enquiry Servlet</title>");
+						pw.println("<body>");
+						pw.println("<h1 align=center> Employee Enquiry Servlet</h1>");
+					
+						pw.println("</body>");
+						pw.println("<table border=5 cellspacing=10 cellpadding=10>");
+						pw.println("<tr>");
+							pw.println("<td> EMPNO </td>");	pw.println("<td>"+empno+"</td>");
+						pw.println("</tr>");
+					
+						pw.println("<tr>");
+							pw.println("<td> EMPNAME </td>");	pw.println("<td>"+ename+"</td>");
+						pw.println("</tr>");
 				
-				System.out.println("EMPNO  : "+empno);
-				System.out.println("NAME   : "+ename);
-				System.out.println("JOB    : "+job);
-				System.out.println("MGR    : "+mgr);
-				System.out.println("DOJ    : "+date);
-				System.out.println("SAL    : "+salary);
-				System.out.println("COMM   : "+comm);
-				System.out.println("DEPTNO : "+deptno);
-				System.out.println("-----------------------------");
-		
-			pw.println("<html>");
+						pw.println("<tr>");
+							pw.println("<td> JOB </td>");	pw.println("<td>"+job+"</td>");
+						pw.println("</tr>");
+					
+						pw.println("<tr>");
+							pw.println("<td> MANAGER </td>");	pw.println("<td>"+mgr+"</td>");
+						pw.println("</tr>");
+					
+						pw.println("<tr>");
+							pw.println("<td> JOINING </td>");	pw.println("<td>"+date+"</td>");
+						pw.println("</tr>");
+					
+						pw.println("<tr>");
+							pw.println("<td> SALARY </td>");	pw.println("<td>"+salary+"</td>");
+						pw.println("</tr>");
+					
+						pw.println("<tr>");
+							pw.println("<td> COMM </td>");	pw.println("<td>"+comm+"</td>");
+						pw.println("</tr>");
+					
+						pw.println("<tr>");
+							pw.println("<td> DEPTNO </td>");	pw.println("<td>"+deptno+"</td>");
+						pw.println("</tr>");	
+				}
+				else {
+					pw.println("<tr>");
+						pw.println("<td> EMPNO NOT FOUND : "+eno+"</td>");	
+					pw.println("</tr>");
+				}
+				pw.println("</table>");
+				pw.println("<a href='http://localhost:8085/MyEmployeeProject/'>Home</a>");
+				pw.println("</html>");
+			}
+			else if(whatIsClicked.equals("Find All Employees")) {
+				st = conn.createStatement();
+				rs = st.executeQuery("select * from emp");
+				//4 process it
+				
+				pw.println("<html>");
 				pw.println("<title>Employee Enquiry Servlet</title>");
 				pw.println("<body>");
-					pw.println("<h1 align=center> Employee Enquiry Servlet</h1>");
+				pw.println("<h1 align=center> Employee Enquiry Servlet</h1>");
+				pw.println("<table align=center  border=5 cellspacing=10 cellpadding=10>");
+				pw.println("<th> EMPNO </th>");	
+				pw.println("<th> EMPNAME </th>");
+				pw.println("<th> JOB </th>");	
+				pw.println("<th> MANAGER </th>");
+				pw.println("<th> JOINING </th>");
+				pw.println("<th> SALARY </th>");
+				pw.println("<th> COMM </th>");	
+				pw.println("<th> DEPTNO </th>");
+				
+				while(rs.next()) 
+				{ //if the record is found
+					int empno 	 = rs.getInt(1);
+					String ename = rs.getString(2);
+					String job   =  rs.getString(3);
+					int mgr      =  rs.getInt(4);
+					java.sql.Date date = rs.getDate(5);
+					float salary = rs.getFloat(6);
+					float comm   = rs.getFloat(7);
+					int deptno   = rs.getInt(8);
+					System.out.println("EMPNO  : "+empno);
+					System.out.println("NAME   : "+ename);
+					System.out.println("JOB    : "+job);
+					System.out.println("MGR    : "+mgr);
+					System.out.println("DOJ    : "+date);
+					System.out.println("SAL    : "+salary);
+					System.out.println("COMM   : "+comm);
+					System.out.println("DEPTNO : "+deptno);
+					System.out.println("-----------------------------");	
+				
 					
+						pw.println("<tr>");
+								pw.println("<td>"+empno+"</td>");
+								pw.println("<td>"+ename+"</td>");
+								pw.println("<td>"+job+"</td>");
+								pw.println("<td>"+mgr+"</td>");
+								pw.println("<td>"+date+"</td>");
+								pw.println("<td>"+salary+"</td>");
+								pw.println("<td>"+comm+"</td>");
+								pw.println("<td>"+deptno+"</td>");
+						pw.println("</tr>");	
+				}
+				
+				pw.println("</table>");
+				pw.println("<a href='http://localhost:8085/MyEmployeeProject/'>Home</a>");
 				pw.println("</body>");
-				
-				pw.println("<table border=5 cellspacing=10 cellpadding=10>");
-				
-					pw.println("<tr>");
-						pw.println("<td> EMPNO </td>");	pw.println("<td>"+empno+"</td>");
-					pw.println("</tr>");
-					
-					pw.println("<tr>");
-						pw.println("<td> EMPNAME </td>");	pw.println("<td>"+ename+"</td>");
-					pw.println("</tr>");
-				
-					pw.println("<tr>");
-						pw.println("<td> JOB </td>");	pw.println("<td>"+job+"</td>");
-					pw.println("</tr>");
-					
-					pw.println("<tr>");
-						pw.println("<td> MANAGER </td>");	pw.println("<td>"+mgr+"</td>");
-					pw.println("</tr>");
-					
-					pw.println("<tr>");
-						pw.println("<td> JOINING </td>");	pw.println("<td>"+date+"</td>");
-					pw.println("</tr>");
-					
-					pw.println("<tr>");
-						pw.println("<td> SALARY </td>");	pw.println("<td>"+salary+"</td>");
-					pw.println("</tr>");
-					
-					pw.println("<tr>");
-						pw.println("<td> COMM </td>");	pw.println("<td>"+comm+"</td>");
-					pw.println("</tr>");
-					
-					pw.println("<tr>");
-						pw.println("<td> DEPTNO </td>");	pw.println("<td>"+deptno+"</td>");
-					pw.println("</tr>");	
+				pw.println("</html>");
 			}
-			else {
-				pw.println("<tr>");
-					pw.println("<td> EMPNO NOT FOUND : "+eno+"</td>");	
-				pw.println("</tr>");
-			}
-			
-			pw.println("</table>");
-			
-			pw.println("<a href='http://localhost:8085/MyEmployeeProject/'>Home</a>");
-			pw.println("</html>");
-		}
+		} // end of try
 		catch(Exception e) {
 			System.out.println("Some problem : "+e);
 		}
 	}
+	
 
 }
